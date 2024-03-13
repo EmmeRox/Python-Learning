ecb8a8d8daaaac9e4512d54f01d2b995d2feece4
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2024-03-13 17:49:07 -0500

    more practice with functions in Python

--- PyPractice.py
+++ PyPractice.py
@@ -191,6 +191,13 @@ else:
 #for simple loops or iterating over iterable objects, for loops are great
 #dont know how long it will take to loop through something, use while loops
   
+#can make your own custom functions using def (define)
+def say_hello():
+   print('Hello')
+
+#in order to use the function now, we have to call the function just like in javascript
+say_hello()
+#it is important to define functions before calling it in code, just like in python. Interpreter reads from top to bottom  
 #exercise #1
 
 picture = [
@@ -238,3 +245,25 @@ def multiply(n):
         num = 5 ** float(digit) 
     return n * num
         
+#exercise from ZTM
+
+def checkDriverAge(age):
+    age = 0
+    age = input('What is your age?: "')
+    if int(age) < 18:
+	    print("Sorry, you are too young to drive this car. Powering off")
+    elif int(age) > 18:
+	    print("Powering On. Enjoy the ride!");
+    elif int(age) == 18:
+	    print("Congratulations on your first year of driving. Enjoy the ride!")
+
+checkDriverAge(13)
+
+
+#1. Wrap the above code in a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. 
+# Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+
+#2 Instead of using the input(). Now, make the checkDriverAge() function accept an argument of age, so that if you enter:
+#checkDriverAge(92);
+#it returns "Powering On. Enjoy the ride!"
+#also make it so that the default age is set to 0 if no argument is given.
